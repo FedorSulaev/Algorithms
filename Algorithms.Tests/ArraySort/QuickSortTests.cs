6ae9c118d41f11682d525e5c975c62528cff3392
FedorSulaev <fedor.sulaev@outlook.com>
2014-05-13 12:58:50 +0400

    QuickSort

---
+++ Algorithms.Tests/ArraySort/QuickSortTests.cs
@@ -0,0 +1,30 @@
+﻿using Algorithms.ArraySort;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algorithms.Tests.ArraySort
+{
+	[TestClass]
+	public class QuickSortTests
+	{
+		private QuickSort _quickSort = new QuickSort();
+
+		[TestMethod]
+		public void Sort_1Element_Return1Element()
+		{
+			int[] input = { 1 };
+			CollectionAssert.AreEqual(input, _quickSort.Sort(input));
+		}
+
+		[TestMethod]
+		public void Sort_2UnsortedElements_ReturnSame2SortedElements()
+		{
+			CollectionAssert.AreEqual(new[] { 1, 2 }, _quickSort.Sort(new[] { 2, 1 }));
+		}
+
+		[TestMethod]
+		public void Sort_5UnsortedElements_ReturnSame5SortedElements()
+		{
+			CollectionAssert.AreEqual(new[] { 1, 2, 3, 4, 5 }, _quickSort.Sort(new[] { 3, 4, 2, 5, 1 }));
+		}
+	}
+}
---
+++ Algorithms/ArraySort/QuickSort.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+
+namespace Algorithms.ArraySort
+{
+	public class QuickSort
+	{
+		public ICollection Sort(int[] input)
+		{
+			SortRecursive(input, 0, input.Length - 1);
+			return input;
+		}
+
+		private void SortRecursive(int[] elements, int left, int right)
+		{
+			int i = left, j = right;
+			int pivot = elements[(left + right)/2];
+
+			while (i <= j)
+			{
+				while (elements[i] < pivot)
+				{
+					i++;
+				}
+				while (elements[j] > pivot)
+				{
+					j--;
+				}
+				if (i <= j)
+				{
+					int tmp = elements[i];
+					elements[i] = elements[j];
+					elements[j] = tmp;
+					i++;
+					j--;
+				}
+			}
+
+			if (left < j)
+			{
+				SortRecursive(elements, left, j);
+			}
+			if (i < right)
+			{
+				SortRecursive(elements, i, right);
+			}
+		}
+	}
+}
